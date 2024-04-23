971ff56c12c684d97f56c535ac82ea3b9e8bea79
nopa <110712245+Nopathee@users.noreply.github.com>
2024-04-23 20:16:20 +0700

    create Proj

---
+++ src/main/java/com/example/int221backend/Int221BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.int221backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Int221BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Int221BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/int221backend/Int221BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.int221backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Int221BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
