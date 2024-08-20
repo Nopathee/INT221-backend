2661a0ea80c65987ecaa22a9b036902f3fd5e73c
nopa <110712245+Nopathee@users.noreply.github.com>
2024-08-20 20:44:28 +0700

    crossOrigin

--- src/main/java/com/example/int221backend/user_controllers/UserController.java
+++ src/main/java/com/example/int221backend/user_controllers/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
+@CrossOrigin(origins = {"http://localhost:5173","http://ip23ssi3.sit.kmutt.ac.th","http://intproj23.sit.kmutt.ac.th"})
 @RestController
 @RequestMapping("/login")
 @AllArgsConstructor
