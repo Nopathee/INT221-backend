7122615c5e5e4d87d9664de2d7d5130b137839be
Aukky1 <thitiphat.tana@kmutt.ac.th>
2024-10-07 08:20:00 +0700

    add owner

--- src/main/java/com/example/int221backend/dtos/BoardIdDTO.java
+++ src/main/java/com/example/int221backend/dtos/BoardIdDTO.java
@@ -1,5 +1,6 @@
 package com.example.int221backend.dtos;
 
+import com.example.int221backend.entities.local.UserLocal;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,4 +12,5 @@ public class BoardIdDTO {
     private String id;
     private String boardName;
     private String visibility;
+    private UserLocal owner;
 }
